13ae7ec50ac2d56701f5866311c22e77680e17f8
Katelynn Cowan <kcowan@twu.edu>
2021-03-01 08:54:55 -0600

    Added files from UT starter code (DB, Public and server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const dbJSON = require("./db/db.json");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./public"));
+
+// Routes
+// =============================================================
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
